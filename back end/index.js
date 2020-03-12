18aefc860a2fd7bff2d3ce46e38226d4269129dc
Chris <kockonoop@gmail.com>
2020-03-12 07:49:35 -0700

    about-agregado

--- back end/index.js
+++ back end/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
+const PORT = process.env.PORT || 4000;
 
 // crear el servidor
 const app = express();
@@ -14,8 +15,6 @@ app.use(cors());
 // Habilitar express.json
 app.use( express.json({ extended: true }));
 
-// puerto de la app
-const port = process.env.PORT || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -24,6 +23,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
-app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
